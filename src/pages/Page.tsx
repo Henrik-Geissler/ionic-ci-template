3c225026a581931ca865264572573f66d76f06ce
H <h@henriks-mbp.speedport.ip>
2020-04-07 17:12:25 +0200

    feat(photoGallery):Add PhotoGallery

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,3 +1,4 @@
+import {camera, trash, close} from 'ionicons/icons'
 import {
   IonButtons,
   IonContent,
@@ -6,12 +7,23 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
 } from '@ionic/react'
 import React from 'react'
 import {RouteComponentProps} from 'react-router'
 import ExploreContainer from '../components/ExploreContainer'
+import {usePhotoGallery} from '../hooks/usePhotoGallery'
 import './Page.css'
 function Page({match}: RouteComponentProps<{name: string}>): JSX.Element {
+  const {photos, takePhoto} = usePhotoGallery()
+
   return (
     <IonPage>
       <IonHeader>
@@ -29,7 +41,21 @@ function Page({match}: RouteComponentProps<{name: string}>): JSX.Element {
             <IonTitle size="large">{match.params.name}</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <ExploreContainer name={match.params.name} />
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   )
