6caaef400ddb4cef01d2d6a5fb209e579ebc430c
H <h@henriks-mbp.speedport.ip>
2020-04-07 10:55:16 +0200

    feat(cucumber): Create Cucumber config

---
+++ cucumber.js
@@ -0,0 +1,12 @@
+const common = [
+  'features/features/**/*.feature',
+  '--format progress-bar',
+  '--format node_modules/cucumber-pretty',
+  '--retry 4',
+  '--exit',
+  'PORT=3000',
+].join(' ')
+
+module.exports = {
+  default: common,
+}
