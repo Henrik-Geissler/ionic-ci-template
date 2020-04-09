08cc2a8bc9d71e4ba0840c1de9c01e675d844703
H <h@henriks-mbp.speedport.ip>
2020-04-09 11:07:43 +0200

    refactor(android): add empty android project

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,32 @@
-import Menu from './components/Menu'
-import Page from './pages/Page'
-import Detail from './pages/Detail'
-import React, {useState} from 'react'
-import {IonApp, IonRouterOutlet, IonSplitPane} from '@ionic/react'
-import {IonReactRouter} from '@ionic/react-router'
-import {Redirect, Route} from 'react-router-dom'
-import PropTypes from 'prop-types'
-import {images, square, triangle} from 'ionicons/icons'
+import Menu from './components/Menu';
+import Page from './pages/Page';
+import React, { useState } from 'react';
+import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Redirect, Route } from 'react-router-dom';
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css'
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css'
-import '@ionic/react/css/structure.css'
-import '@ionic/react/css/typography.css'
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css'
-import '@ionic/react/css/float-elements.css'
-import '@ionic/react/css/text-alignment.css'
-import '@ionic/react/css/text-transformation.css'
-import '@ionic/react/css/flex-utils.css'
-import '@ionic/react/css/display.css'
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import './theme/variables.scss'
-import './theme/glitch.scss'
-import './theme/neon.scss'
-import './theme/attention.scss'
-import './theme/shimmer.scss'
-import './theme/frozen.scss'
-import './theme/focus.scss'
+import './theme/variables.css';
 
-function App(): JSX.Element {
-  const [selectedPage, setSelectedPage] = useState('')
+const App: React.FC = () => {
+
+  const [selectedPage, setSelectedPage] = useState('');
 
   return (
     <IonApp>
@@ -42,40 +34,16 @@ function App(): JSX.Element {
         <IonSplitPane contentId="main">
           <Menu selectedPage={selectedPage} />
           <IonRouterOutlet id="main">
-            <Route
-              path="/page/:name"
-              render={function (props): JSX.Element {
-                setSelectedPage(props.match.params.name)
-                return <Page {...props} />
-              }}
-              exact={true}
-            />
-
-            <Route
-              path="/style/:name"
-              render={function (props): JSX.Element {
-                setSelectedPage(props.match.params.name)
-                return <Detail {...props} />
-              }}
-              exact={true}
-            />
-            <Route
-              path="/"
-              render={(): JSX.Element => <Redirect to="/page/Inbox" />}
-              exact={true}
-            />
+            <Route path="/page/:name" render={(props) => {
+              setSelectedPage(props.match.params.name);
+              return <Page {...props} />;
+            }} exact={true} />
+            <Route path="/" render={() => <Redirect to="/page/Inbox" />} exact={true} />
           </IonRouterOutlet>
         </IonSplitPane>
       </IonReactRouter>
     </IonApp>
-  )
-}
+  );
+};
 
-App.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-    }),
-  }),
-}
-export default App
+export default App;
--- src/components/ExploreContainer.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {render} from '@testing-library/react'
-import ExploreContainer from './ExploreContainer'
-
-test('renders without crashing', () => {
-  const str = 'Test'
-  const {baseElement} = render(<ExploreContainer name={str} />)
-  expect(baseElement).toBeDefined()
-})
